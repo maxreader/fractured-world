6044baea59db7df071977d0c2d1773f0285a52a6
60635199 <60635199+maxreader@users.noreply.github.com>
2020-07-23 22:58:17 -0500

    added debug profiling

---
+++ control.lua
@@ -0,0 +1,10 @@
+--[[script.on_event(defines.events.on_chunk_generated, function(event)
+    local radius = 30
+    local position = event.position
+    local surface = event.surface
+    local thisProfiler = game.create_profiler()
+    surface.request_to_generate_chunks(position, radius)
+    surface.force_generate_chunk_requests()
+    thisProfiler.stop()
+    game.print(thisProfiler)
+end)]] 
