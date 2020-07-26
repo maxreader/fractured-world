387b76c09df3df7f5f5e35ab11999b5752ae35ef
60635199 <60635199+maxreader@users.noreply.github.com>
2020-07-26 15:11:55 -0500

    Fixed data.lua requires

--- data.lua
+++ data.lua
@@ -1,9 +1,9 @@
 local tne = require("noise").to_noise_expression
 local preset_data = require('prototypes.preset-data')
-local fne = require("prototypes.noise-expressions")
+local fne = require("prototypes.fractured-noise-expressions")
 local make_cartesian_preset = fne.make_cartesian_preset
 local make_voronoi_preset = fne.make_voronoi_preset
-local make_preset = require("map-gen-presets")
+local make_preset = require("prototypes.map-gen-presets")
 
 local function make_world(name, args)
     if args.cartesian then
