f80e4b3932ca9a2991e71e7b462d239b264ec8e7
60635199 <60635199+maxreader@users.noreply.github.com>
2021-05-02 21:48:31 -0500

    hardCrafting support, better oil-like defaults

--- raw-data/resource-data.lua
+++ raw-data/resource-data.lua
@@ -13,22 +13,10 @@ local pyRock = {
 }
 local rawResourceData = {
     base = {
-        ["iron-ore"] = {
-            base_density = 10,
-            starting_rq_factor_multiplier = 1.5
-        },
-        ["copper-ore"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.2
-        },
-        ["coal"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.1
-        },
-        ["stone"] = {
-            base_density = 4,
-            starting_rq_factor_multiplier = 1.1
-        },
+        ["iron-ore"] = {base_density = 10, starting_rq_factor_multiplier = 1.5},
+        ["copper-ore"] = {base_density = 8, starting_rq_factor_multiplier = 1.2},
+        ["coal"] = {base_density = 8, starting_rq_factor_multiplier = 1.1},
+        ["stone"] = {base_density = 4, starting_rq_factor_multiplier = 1.1},
         ["uranium-ore"] = {
             base_density = 0.9,
             base_spots_per_km2 = 1.25,
@@ -70,10 +58,7 @@ local rawResourceData = {
             random_spot_size_minimum = 2,
             random_spot_size_maximum = 4
         },
-        ["tin-ore"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.2
-        },
+        ["tin-ore"] = {base_density = 8, starting_rq_factor_multiplier = 1.2},
         ["tungsten-ore"] = {base_density = 8},
         ["zinc-ore"] = {base_density = 4}
 
@@ -87,32 +72,14 @@ local rawResourceData = {
             additional_richness = 2500,
             random_probability = 1 / 48
         },
-        ["angels-ore1"] = {
-            base_density = 10,
-            starting_rq_factor_multiplier = 1.5
-        },
-        ["angels-ore2"] = {
-            base_density = 7,
-            starting_rq_factor_multiplier = 1.1
-        },
+        ["angels-ore1"] = {base_density = 10, starting_rq_factor_multiplier = 1.5},
+        ["angels-ore2"] = {base_density = 7, starting_rq_factor_multiplier = 1.1},
         ["angels-ore3"] = {base_density = 10},
         starting_rq_factor_multiplier = 1.5,
-        ["angels-ore4"] = {
-            base_density = 7,
-            starting_rq_factor_multiplier = 1.1
-        },
-        ["angels-ore5"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.2
-        },
-        ["angels-ore6"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.2
-        },
-        ["coal"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.1
-        },
+        ["angels-ore4"] = {base_density = 7, starting_rq_factor_multiplier = 1.1},
+        ["angels-ore5"] = {base_density = 8, starting_rq_factor_multiplier = 1.2},
+        ["angels-ore6"] = {base_density = 8, starting_rq_factor_multiplier = 1.2},
+        ["coal"] = {base_density = 8, starting_rq_factor_multiplier = 1.1},
         ["crude-oil"] = {
             base_density = 8,
             base_spots_per_km2 = 1.8,
@@ -165,38 +132,14 @@ local rawResourceData = {
         }
     },
     DyWorld = {
-        ["stone"] = {
-            base_density = 24,
-            starting_rq_factor_multiplier = 1.35
-        },
-        ["coal"] = {
-            base_density = 12,
-            starting_rq_factor_multiplier = 1.25
-        },
-        ["iron-ore"] = {
-            base_density = 15,
-            starting_rq_factor_multiplier = 1.5
-        },
-        ["copper-ore"] = {
-            base_density = 13,
-            starting_rq_factor_multiplier = 1.45
-        },
-        ["nickel-ore"] = {
-            base_density = 12,
-            starting_rq_factor_multiplier = 1.32
-        },
-        ["silver-ore"] = {
-            base_density = 8,
-            starting_rq_factor_multiplier = 1.2
-        },
-        ["tin-ore"] = {
-            base_density = 9,
-            starting_rq_factor_multiplier = 1.27
-        },
-        ["gold-ore"] = {
-            base_density = 5,
-            starting_rq_factor_multiplier = 1.41
-        },
+        ["stone"] = {base_density = 24, starting_rq_factor_multiplier = 1.35},
+        ["coal"] = {base_density = 12, starting_rq_factor_multiplier = 1.25},
+        ["iron-ore"] = {base_density = 15, starting_rq_factor_multiplier = 1.5},
+        ["copper-ore"] = {base_density = 13, starting_rq_factor_multiplier = 1.45},
+        ["nickel-ore"] = {base_density = 12, starting_rq_factor_multiplier = 1.32},
+        ["silver-ore"] = {base_density = 8, starting_rq_factor_multiplier = 1.2},
+        ["tin-ore"] = {base_density = 9, starting_rq_factor_multiplier = 1.27},
+        ["gold-ore"] = {base_density = 5, starting_rq_factor_multiplier = 1.41},
         ["lead-ore"] = {base_density = 12},
         ["cobalt-ore"] = {base_density = 10},
         ["arditium-ore"] = {base_density = 15},
@@ -205,41 +148,13 @@ local rawResourceData = {
         ["neutronium-ore"] = {base_density = 25}
     },
     pyrawores = {
-        borax = {
-            base_density = (20 / 3),
-            base_spots_per_km2 = 3,
-            additional_richness = 10
-        },
-        ["ore-quartz"] = {
-            base_density = 15,
-            base_spots_per_km2 = 2.5,
-            additional_richness = 300
-        },
-        ["raw-coal"] = {
-            base_density = 15,
-            base_spots_per_km2 = 2.5,
-            additional_richness = 300
-        },
-        ["ore-tin"] = {
-            base_density = 15,
-            base_spots_per_km2 = 2.5,
-            additional_richness = 300
-        },
-        ["ore-titanium"] = {
-            base_density = 15,
-            base_spots_per_km2 = 2.5,
-            additional_richness = 300
-        },
-        niobium = {
-            base_density = 15,
-            base_spots_per_km2 = 1.875,
-            additional_richness = 500
-        },
-        ["ore-lead"] = {
-            base_density = 15,
-            base_spots_per_km2 = 1.625,
-            additional_richness = 300
-        },
+        borax = {base_density = (20 / 3), base_spots_per_km2 = 3, additional_richness = 10},
+        ["ore-quartz"] = {base_density = 15, base_spots_per_km2 = 2.5, additional_richness = 300},
+        ["raw-coal"] = {base_density = 15, base_spots_per_km2 = 2.5, additional_richness = 300},
+        ["ore-tin"] = {base_density = 15, base_spots_per_km2 = 2.5, additional_richness = 300},
+        ["ore-titanium"] = {base_density = 15, base_spots_per_km2 = 2.5, additional_richness = 300},
+        niobium = {base_density = 15, base_spots_per_km2 = 1.875, additional_richness = 500},
+        ["ore-lead"] = {base_density = 15, base_spots_per_km2 = 1.625, additional_richness = 300},
         ["ore-aluminum"] = {
             base_density = 10,
             base_spots_per_km2 = 0.1875,
@@ -250,16 +165,8 @@ local rawResourceData = {
             base_spots_per_km2 = 0.1875,
             additional_richness = 10
         },
-        ["ore-nickel"] = {
-            base_density = 10,
-            base_spots_per_km2 = 0.1875,
-            additional_richness = 10
-        },
-        ["ore-zinc"] = {
-            base_density = 10,
-            base_spots_per_km2 = 0.1875,
-            additional_richness = 10
-        },
+        ["ore-nickel"] = {base_density = 10, base_spots_per_km2 = 0.1875, additional_richness = 10},
+        ["ore-zinc"] = {base_density = 10, base_spots_per_km2 = 0.1875, additional_richness = 10},
 
         ["salt-rock"] = {
             base_density = 800 / 3,
@@ -286,22 +193,28 @@ local rawResourceData = {
         ["quartz-rock"] = pyRock,
         ["tin-rock"] = pyRock,
         ["titanium-rock"] = pyRock,
-        ["volcanic-pipe"] = {
-            random_probability = pyRockFreq
-        },
+        ["volcanic-pipe"] = {random_probability = pyRockFreq},
         ["regolites"] = {random_probability = pyRockFreq},
-        ["rare-earth-bolide"] = {
-            random_probability = pyRockFreq
-        },
-        ["phosphate-rock"] = {
-            random_probability = pyRockFreq
-        },
+        ["rare-earth-bolide"] = {random_probability = pyRockFreq},
+        ["phosphate-rock"] = {random_probability = pyRockFreq},
         ["sulfur-patch"] = {random_probability = pyRockFreq},
         ["oil-mk01"] = {random_probability = pyRockFreq},
         ["oil-mk02"] = {random_probability = pyRockFreq},
         ["oil-mk03"] = {random_probability = pyRockFreq},
         ["oil-mk04"] = {random_probability = pyRockFreq},
         ["tar-patch"] = {random_probability = pyRockFreq}
+    },
+    hardCrafting = {
+        ["oil-sand"] = {
+            base_density = 8.2,
+            base_spots_per_km2 = 1.8,
+            random_spot_size_minimum = 1,
+            random_spot_size_maximum = 1,
+            additional_richness = 220000,
+            random_probability = 1 / 48
+        },
+        ["rich-iron-ore"] = {base_density = 6},
+        ["rich-copper-ore"] = {base_density = 4.5}
     }
 }
 for k, v in pairs(rawResourceData) do if mods[k] then fractured_world:set_resource_data(k, v) end end
