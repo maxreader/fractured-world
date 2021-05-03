208f9a390ef19ee2a891133f672eb549bb2fc5ab
60635199 <60635199+maxreader@users.noreply.github.com>
2021-05-02 21:26:42 -0500

    Quick startup preset setting added

--- data.lua
+++ data.lua
@@ -10,18 +10,11 @@ end]]
 require("raw-data.cartesian-functions")
 
 local presets = require("raw-data.preset-data")
-if settings.startup["fractured-world-use-quick-startup"]
-    .value then
-    local quick_preset =
-        settings.startup["fractured-world-quick-startup-preset"]
-            .value
-    fractured_world:add_preset_data(quick_preset,
-                                    presets[quick_preset])
+if settings.startup["fractured-world-use-quick-startup"].value then
+    local quick_preset = settings.startup["fractured-world-quick-startup-preset"].value
+    fractured_world:add_preset_data(quick_preset, presets[quick_preset])
 else
-
-    for k, v in pairs(presets) do
-        fractured_world:add_preset_data(k, v)
-    end
+    for k, v in pairs(presets) do fractured_world:add_preset_data(k, v) end
 end
 
 require("raw-data.resource-data")
--- settings.lua
+++ settings.lua
@@ -2,7 +2,8 @@ local presets = require("raw-data.preset-data")
 fractured_world = fractured_world or {}
 fractured_world.allowed_presets = fractured_world.allowed_presets or {}
 function fractured_world.add_preset_to_settings(self, preset_name)
-    table.insert(self.allowed_presets, preset_name)
+    local localised_name = preset_name
+    table.insert(self.allowed_presets, localised_name)
 end
 
 for name, _ in pairs(presets) do fractured_world:add_preset_to_settings(name) end
