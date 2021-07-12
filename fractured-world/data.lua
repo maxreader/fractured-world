c70620e9128753a81ef409bc6197f69add635386
60635199 <60635199+maxreader@users.noreply.github.com>
2021-07-12 13:51:53 -0500

    Reset quick preset setting

--- fractured-world/data.lua
+++ fractured-world/data.lua
@@ -11,8 +11,11 @@ local presets = require("raw-data.preset-data")
 if settings.startup["fractured-world-use-quick-startup"].value then
     local quick_preset = settings.startup["fractured-world-quick-startup-preset"].value
     fractured_world:add_preset_data(quick_preset, presets[quick_preset])
-    fractured_world:add_preset_data("diamonds", presets["diamonds"])
-    fractured_world:add_preset_data("fractured-resources", presets["fractured-resources"])
+    if quick_preset == "fractured-resources" then
+        for k, v in pairs({"fractured-resources", "circles", "squares", "diamonds"}) do
+            fractured_world:add_preset_data(v, presets[v])
+        end
+    end
 else
     for k, v in pairs(presets) do fractured_world:add_preset_data(k, v) end
 end
